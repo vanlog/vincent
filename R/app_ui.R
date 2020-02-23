aa5390cf2f203a78807b4f0ef949c8819e23eaa7
Andrea Melloncelli <andrea.melloncelli@gmail.com>
2020-02-23 17:58:13 +0100

    Add Shinydashboard

--- R/app_ui.R
+++ R/app_ui.R
@@ -1,11 +1,31 @@
 #' @import shiny
+#' @import shinydashboard
+#' 
 app_ui <- function() {
   tagList(
     # Leave this function for adding external resources
     golem_add_external_resources(),
     # List the first level UI elements here 
-    fluidPage(
-      h1("vincent")
+    dashboardPage(
+      dashboardHeader(title = "Hi, i'm Vincent"),
+      ## Sidebar content
+      dashboardSidebar(
+        sidebarMenu(
+          menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
+          menuItem("Journal", tabName = "journal", icon = icon("calendar"))
+        )
+      ),
+      ## Body content
+      dashboardBody(
+        tabItems(
+          
+          # First tab content
+          tabItem(tabName = "dashboard"),
+          
+          #Second tab content
+          tabItem(tabName = "journal")        
+        )
+      )
     )
   )
 }
@@ -16,7 +36,7 @@ golem_add_external_resources <- function(){
   addResourcePath(
     'www', system.file('app/www', package = 'vincent')
   )
- 
+  
   tags$head(
     golem::activate_js(),
     golem::favicon()
