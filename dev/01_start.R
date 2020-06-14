cd2d55052f0a02bf5e4a640ac73ad51ba8e0a068
Andrea Melloncelli <andrea.melloncelli@gmail.com>
2020-06-14 21:49:32 +0200

    package init complete

--- dev/01_start.R
+++ dev/01_start.R
@@ -26,24 +26,26 @@ golem::set_golem_options()
 ## 
 ## If you want to use the MIT license, README, code of conduct, lifecycle badge, and news
 
-usethis::use_mit_license( name = "Golem User" )  # You can set another license here
-usethis::use_readme_rmd( open = FALSE )
-usethis::use_code_of_conduct()
-usethis::use_lifecycle_badge( "Experimental" )
-
-usethis::use_news_md( open = FALSE )
-usethis::use_git()
+usethis::use_lgpl_license( name = "Andrea Melloncelli" )  # You can set another license here
+usethis::use_readme_md( open = FALSE )
+# usethis::use_code_of_conduct()
+# usethis::use_lifecycle_badge( "Experimental" )
+# 
+# usethis::use_news_md( open = FALSE )
+# usethis::use_git()
 
 ## 1.3 - Add a data-raw folder
 ## 
 ## If you have data in your package
-usethis::use_data_raw( name = "my_dataset", open = FALSE ) # Change "my_dataset"
+# usethis::use_data_raw( name = "my_dataset", open = FALSE ) # Change "my_dataset"
 
 ## 1.4 - Init Tests
 ## 
 ## Create a template for tests
 
 golem::use_recommended_tests()
+## or simply:
+# devtools::use_testthat()
 
 ## 1.5 : Use Recommended Package
 
@@ -52,12 +54,12 @@ golem::use_recommended_deps()
 ## 1.6 Add various tools
 
 # If you want to change the favicon (default is golem's one)
-golem::remove_favicon()
-golem::use_favicon() # path = "path/to/ico". Can be an online file. 
+# golem::remove_favicon()
+# golem::use_favicon() # path = "path/to/ico". Can be an online file. 
 
 # Add helper functions 
-golem::use_utils_ui()
-golem::use_utils_server()
+# golem::use_utils_ui()
+# golem::use_utils_server()
 
 # You're now set! 
 # go to dev/02_dev.R
